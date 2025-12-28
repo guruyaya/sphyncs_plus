ebb55e344a4297b1e83dcc7ce656ae6dce0ba927
yair Eshel <yair@polywizz.co.il>
2025-12-28 10:41:30 +0200

    Cleanup

--- spyncs_plus/helpers/key_generators/csprng.py
+++ spyncs_plus/helpers/key_generators/csprng.py
@@ -20,11 +20,6 @@ class CSPRNGKeyGenerator(GenericKeyGenerator):
 
     jump_size = jump * self.key_size_bytes
     self._instance.randbytes(jump_size)
-    # max_jump = 2**27
-
-    # for jmp in range(jump_size // max_jump):
-    #   self._instance.randbytes(max_jump)
-    # self._instance.randbytes(jump_size % max_jump)
-
+    
   def _get_rand_key(self) -> bytes:
     return self._instance.randbytes(self.key_size_bytes)
