a69b3afec78acebdd406b897747c5dd0709a29d0
yair Eshel <yair@polywizz.co.il>
2026-01-03 17:05:58 +0200

    Improved exception

--- spyncs_plus/components/shpincs_tree/secret.py
+++ spyncs_plus/components/shpincs_tree/secret.py
@@ -1,4 +1,3 @@
-from typing import Self
 from spyncs_plus.helpers.hashers import GenericHasher
 from spyncs_plus.helpers.key_generators import GenericKeyGenerator
 from spyncs_plus.components.wots_plus import WotsPlusSecret
@@ -31,14 +30,14 @@ class SphincsTreeSecret(SphincsTreePublic):
 
     self.public_key = hasher.concatenate_hash(self.levels[-1][0])
 
-  def sign(self, message: bytes, key_num: int) -> SphincsTreeProof:
+  def sign(self, message: bytes, key_num: int, allready_hashed=False) -> SphincsTreeProof:
     if key_num >= 2 ** self.num_levels:
-      raise Exception("Key number is out of range")
+      raise Exception(f"Key number {key_num} is out of range (must be lower than ({2 ** self.num_levels}))")
 
     pair_num_pos = []
 
     wots_key = self.base_level[key_num // 2][key_num % 2]
-    signature = wots_key.sign(message)
+    signature = wots_key.sign(message, allready_hashed=allready_hashed)
 
     for i in range(self.num_levels):
       pair_num = key_num // 2
@@ -48,7 +47,8 @@ class SphincsTreeSecret(SphincsTreePublic):
       key_num = pair_num
 
     message_hash = self.hasher(message)
-    return SphincsTreeProof(message_hash=message_hash, signature=signature, proof_tree=pair_num_pos, public_key=self.public_key)
+    return SphincsTreeProof(message_hash=message_hash, signature=signature, proof_tree=pair_num_pos, 
+                            public_key=self.public_key)
 
   def sign_level_down(self, level_down: SphincsTreePublic, key_num: int):
     level_down.level_up_proof = self.sign(level_down.public_key, key_num)
